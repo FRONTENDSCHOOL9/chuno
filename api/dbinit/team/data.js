70ff0cb8e420d59b7179cd98c5e1ec4dbef88c97
ruelline <ruelline91@naver.com>
2024-04-11 14:36:47 +0900

    ✨ API-Client폴더 추가

---
+++ api/dbinit/team/data.js
@@ -0,0 +1,31 @@
+import moment from 'moment';
+
+function getDay(day = 0) {
+  return moment().add(day, 'days').format('YYYY.MM.DD');
+}
+function getTime(day = 0, second = 0) {
+  return moment().add(day, 'days').add(second, 'seconds').format('YYYY.MM.DD HH:mm:ss');
+}
+
+export const initData = async (nextSeq) => {
+  return {
+    // 회원
+    user: [],
+    // 상품
+    product: [],
+    // 주문
+    order: [],
+    // 후기
+    reply: [],
+    // 장바구니
+    cart: [],
+    // 즐겨찾기/북마크
+    bookmark: [],
+    // QnA, 공지사항, 게시판
+    post: [],
+    // 코드
+    code: [],
+    // 설정
+    config: [],
+  };
+};
