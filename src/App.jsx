d568807709a845e024680c88e7d7894ca738dcd3
wonmyoung <wonmyounglee@gmail.com>
2024-04-12 16:23:54 +0900

    버튼 디자인 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { RecoilRoot } from 'recoil';
+import Button1 from './pages/components/Button1';
+import Button2 from './pages/components/Button2';
+import Button3 from './pages/components/Button3';
+import Mypage from './pages/components/Mypage';
 
 
 
 function App() {
   return (
     <RecoilRoot>
-      
+        <Mypage /><br/>
     </RecoilRoot>
   );
 }
---
+++ src/pages/components/Button2.jsx
@@ -0,0 +1,10 @@
+import './button.css'
+
+function Button1(){
+    return(
+        <>
+            <button className='btn2'>플레이리스트 추가하기</button>
+        </>
+    )
+}
+export default Button1;
