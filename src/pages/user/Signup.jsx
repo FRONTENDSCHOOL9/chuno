834a9e2e211d3d6d5dd0a63611bd72eda58545a9
De-Chip <milkyice4567@naver.com>
2024-04-15 15:50:48 +0900

    🚧 css 초기화

--- src/pages/user/Signup.jsx
+++ src/pages/user/Signup.jsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form';
 import useCustomAxios from '@hooks/useCustomAxios.mjs';
 import { useNavigate } from 'react-router-dom';
 import Submit from '@components/Submit';
+import './signup.css';
 
 function Signup() {
   const axios = useCustomAxios();
@@ -24,31 +25,35 @@ function Signup() {
 
       formData.type = 'seller';
 
-//FIXME - 이미지를 업로드하면 회원가입이 안됩니다, 이미지를 보내는 과정에서 뭔가 오류가 있는 것 같은데 잘 모르겠어요... 뭘 어떻게 고쳐야 할까나~
+      //FIXME - 이미지를 업로드하면 회원가입이 안됩니다, 이미지를 보내는 과정에서 뭔가 오류가 있는 것 같은데 잘 모르겠어요... 뭘 어떻게 고쳐야 할까나~
       // 이미지 먼저 업로드
       // 이미지 먼저 업로드
-      if(formData.profileImage.length > 0){ // 프로필 이미지를 추가한 경우
+      if (formData.profileImage.length > 0) {
+        // 프로필 이미지를 추가한 경우
         const imageFormData = new FormData();
         imageFormData.append('attach', formData.profileImage[0]);
-  
+
         const fileRes = await axios('/files', {
           method: 'post',
           headers: {
             // 파일 업로드시 필요한 설정
-            'Content-Type': 'multipart/form-data'
+            'Content-Type': 'multipart/form-data',
           },
-          data: imageFormData
+          data: imageFormData,
         });
-  
+
         // 서버로부터 응답받은 이미지 이름을 회원 정보에 포함
         formData.profileImage = fileRes.data.file.name;
-      }else{
+      } else {
         // profileImage 속성을 제거
         delete formData.profileImage;
       }
 
       const res = await axios.post('/users', formData);
-      alert( res.data.item.name + '님 회원가입이 완료 되었습니다.\n로그인 후에 이용하세요.');
+      alert(
+        res.data.item.name +
+          '님 회원가입이 완료 되었습니다.\n로그인 후에 이용하세요.',
+      );
       navigate('/users/login');
     } catch (err) {
       // AxiosError(네트워크 에러-response가 없음, 서버의 4xx, 5xx 응답 상태 코드를 받았을 때-response 있음)
@@ -64,27 +69,19 @@ function Signup() {
   };
 
   return (
-    <div className="min-w-80 flex-grow flex items-center justify-center">
-      <div className="p-8 shadow-md rounded-lg w-full max-w-md dark:bg-gray-600">
-        <div className="text-center py-4">
-          <h2 className="text-2xl font-bold text-gray-700 dark:text-gray-200">
-            회원가입
-          </h2>
+    <div>
+      <div>
+        <div>
+          <h2>회원가입</h2>
         </div>
 
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 dark:text-gray-200 font-bold mb-2"
-              htmlFor="name"
-            >
-              닉네임
-            </label>
+          <div>
+            <label htmlFor="name">닉네임</label>
             <input
               type="text"
               id="name"
               placeholder="사용할 닉네임을 입력하세요"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 dark:bg-gray-700"
               {...register('name', {
                 required: '닉네임을 입력하세요.',
                 minLength: {
@@ -93,24 +90,14 @@ function Signup() {
                 },
               })}
             />
-            {errors.name && 
-              <p className="ml-2 mt-1 text-sm text-red-500">
-                {errors.name.message}
-              </p>
-            }
+            {errors.name && <p>{errors.name.message}</p>}
           </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 dark:text-gray-200 font-bold mb-2"
-              htmlFor="email"
-            >
-              아이디
-            </label>
+          <div>
+            <label htmlFor="email">아이디</label>
             <input
               type="email"
               id="email"
               placeholder="이메일을 입력하세요"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 dark:bg-gray-700"
               {...register('email', {
                 required: '이메일을 입력하세요.',
                 pattern: {
@@ -119,50 +106,31 @@ function Signup() {
                 },
               })}
             />
-            {errors.email && 
-              <p className="ml-2 mt-1 text-sm text-red-500">
-                {errors.email.message}
-              </p>
-            }
+            {errors.email && <p>{errors.email.message}</p>}
           </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 dark:text-gray-200 font-bold mb-2"
-              htmlFor="password"
-            >
-              비밀번호
-            </label>
+          <div>
+            <label htmlFor="password">비밀번호</label>
             <input
               type="password"
               id="password"
               placeholder="비밀번호를 입력하세요"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 dark:bg-gray-700"
               {...register('password', {
                 required: '비밀번호를 입력하세요.',
               })}
             />
-            {errors.password && (
-              <p className="ml-2 mt-1 text-sm text-red-500">
-                {errors.password.message}
-              </p>
-            )}
+            {errors.password && <p>{errors.password.message}</p>}
           </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 dark:text-gray-200 font-bold mb-2"
-              htmlFor="passwordCheck"
-            >
-              비밀번호 확인
-            </label>
+          <div>
+            <label htmlFor="passwordCheck">비밀번호 확인</label>
             <input
               type="password"
               id="passwordCheck"
               placeholder="비밀번호를 한 번 더 입력하세요"
-              className={`w-full px-3 py-2 border rounded-lg focus:outline-none ${
+              /*               className={`w-full px-3 py-2 border rounded-lg focus:outline-none ${
                 errors.passwordCheck
                   ? 'border-red-500'
                   : 'focus:border-blue-500'
-              } dark:bg-gray-700`}
+              } dark:bg-gray-700`} */
               {...register('passwordCheck', {
                 required: '비밀번호를 다시 확인해주세요.',
                 validate: value =>
@@ -170,29 +138,19 @@ function Signup() {
                   '비밀번호가 일치하지 않습니다.',
               })}
             />
-            {errors.passwordCheck && (
-              <p className="ml-2 mt-1 text-sm text-red-500">
-                {errors.passwordCheck.message}
-              </p>
-            )}
+            {errors.passwordCheck && <p>{errors.passwordCheck.message}</p>}
           </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 dark:text-gray-200 font-bold mb-2"
-              htmlFor="profileImage"
-            >
-              프로필 이미지
-            </label>
+          <div>
+            <label htmlFor="profileImage">프로필 이미지</label>
             <input
               type="file"
               accept="image/*"
               id="profileImage"
               placeholder="이미지를 선택하세요"
-              className="w-full px-3 py-2 border rounded-lg dark:bg-gray-700"
               {...register('profileImage')}
             />
           </div>
-          <div className="mt-14 flex justify-center items-center">
+          <div>
             <Submit>회원가입</Submit>
           </div>
         </form>
