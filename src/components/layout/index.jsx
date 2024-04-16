e4bc29722850f4b8ba912d87bf2293fab489706e
ruelline <ruelline91@naver.com>
2024-04-16 14:36:51 +0900

    🚧

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,17 +1,11 @@
-import Footer from '@components/layout/Footer';
-import Header from '@components/layout/Header';
 import { Outlet } from 'react-router-dom';
-import Login from '../../pages/user/Login';
 import Signup from '../../pages/user/Signup';
 
 function Layout() {
   return (
     <div>
-      <Header />
-      <Signup></Signup>
-      <Login></Login>
       <Outlet />
-      <Footer />
+      <Signup></Signup>
     </div>
   );
 }
