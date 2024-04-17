043cbfd6605b79833ddb1a2c377779a2ebd35bf8
ruelline <ruelline91@naver.com>
2024-04-17 14:52:46 +0900

    🚧 dev 파일로 브랜치 머지중

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,13 +1,9 @@
-import Footer from '@components/layout/Footer';
-import Header from '@components/layout/Header';
 import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <Outlet />
-      <Footer />
     </div>
   );
 }
