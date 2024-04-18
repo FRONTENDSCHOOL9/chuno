c1f246aa69d09d4849646e0d0a79fe558910ce5c
wonmyoung <wonmyounglee@gmail.com>
2024-04-18 09:05:33 +0900

    🚧 메인페이지 마크업

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,20 +1,13 @@
 import Footer from '@components/layout/Footer';
 import Header from '@components/layout/Header';
 import { Outlet } from 'react-router-dom';
-import Button1 from '../../pages/components/Button1';
-import Mypage from '../../pages/components/Mypage';
-import Boardlist from '../../pages/boards/boardlist';
-import Empty from '../../pages/boards/empty';
-import Musicplayer from '../../pages/components/Musicplayer';
-import Button3 from '../../pages/components/Button3';
-import Button2 from '../../pages/components/Button2';
-import Button5 from '../../pages/components/Button5';
-import Button4 from '../../pages/components/Button4';
+import Main from '../../pages/components/Mainpage';
 
 function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Outlet />
+      <Main />
     </div>
   );
 }
--- src/pages/boards/boardlistitem.jsx
+++ src/pages/boards/boardlistitem.jsx
@@ -1,23 +1,22 @@
-import './board.css';
+import styles from './board2.module.css';
 
-
-function Boardlistitem(){
-    return (
-        <>
-        <ul className='wrap_list'>            
-            <li className='listitem'>
-                <div className='thumbnail'>
-                    <img src="" alt="" />
-                </div>
-                <div className='desc'>
-                    <h3>반려동물도 함께 듣는 곡</h3>
-                    <span>by doglike</span>
-                </div>
-                <span className='count'>15곡</span>
-            </li>
-        </ul>
-        </>
-    )
+function Boardlistitem() {
+  return (
+    <>
+      <ul className={styles.wrap_list}>
+        <li className={styles.listitem}>
+          <div className={styles.thumbnail}>
+            <img src="" alt="" />
+          </div>
+          <div className={styles.desc}>
+            <h3>반려동물도 함께 듣는 곡</h3>
+            <span>by doglike</span>
+          </div>
+          <span className={styles.count}>15곡</span>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default Boardlistitem;
