2e8369399842f06e7d3975c709f9d7e20c7db06c
wonmyoung <wonmyounglee@gmail.com>
2024-04-18 11:12:30 +0900

    🚧merge 작업중

--- src/recoil/user/atoms.mjs
+++ src/recoil/user/atoms.mjs
@@ -11,17 +11,3 @@ export const memberState = atom({
   default: null,
   effects: [persistAtom],
 });
-
-import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
-
-const { persistAtom } = recoilPersist({
-  key: 'saveUser',
-  storage: sessionStorage
-});
-
-export const memberState = atom({
-  key: "userState",
-  default: null,
-  effects: [persistAtom]
-});
--- src/routes.jsx
+++ src/routes.jsx
@@ -48,15 +48,3 @@ const router = createBrowserRouter([
 ]);
 
 export default router;
-import Layout from "@components/layout";
-import { createBrowserRouter } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-  },
-  
-]);
-
-export default router;
