818084bac8890d162fc8070b42b9ac62286761c9
wonmyoung <wonmyounglee@gmail.com>
2024-04-19 17:51:00 +0900

    error page 업데이트 jsx 수정 module.css 추가

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,19 +1,23 @@
 import Footer from '@components/layout/Footer';
 import Header from '@components/layout/Header';
 import { useRouteError } from 'react-router-dom';
+import Cookie from '@assets/404error.svg';
+import styles from './errorpage.module.css';
+
 
 function ErrorPage() {
   const err = useRouteError();
   const message =
     err.status === 404
-      ? '존재하지 않는 페이지입니다.'
+      ? '요청하신 페이지를 현재 보여드릴수 없어요.'
       : '예상하지 못한 에러가 발생했습니다.';
   return (
     <>
       <Header />
-      <div>
-        <h2>에러 메세지</h2>
-        <p>{message}</p>
+      <div className={styles.errorpage}>
+        <img src={Cookie} className={styles.errorimage} />
+        {/* <h2>에러 메세지</h2> */}
+        <p className={styles.errormessage}>{message}</p>
       </div>
       <Footer />
     </>
