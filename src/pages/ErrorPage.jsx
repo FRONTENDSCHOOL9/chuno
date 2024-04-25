de8747f804903c07ca96e279c437c753bf501861
ruelline <ruelline91@naver.com>
2024-04-25 09:57:44 +0900

    💄

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,6 @@
 import { useRouteError } from 'react-router-dom';
 import Cookie from '@assets/404error.svg';
 import styles from './errorpage.module.css';
-import ButtonBack from '@components/ButtonBack';
 
 function ErrorPage() {
   const err = useRouteError();
@@ -11,8 +10,7 @@ function ErrorPage() {
       : '예상하지 못한 에러가 발생했습니다.';
   return (
     <>
-      <div id="App">
-        <ButtonBack path={'/main'} />
+      <div className={styles.errorWrap}>
         <div className={styles.errorPage}>
           <img src={Cookie} className={styles.errorImage} />
           {/* <h2>에러 메세지</h2> */}
