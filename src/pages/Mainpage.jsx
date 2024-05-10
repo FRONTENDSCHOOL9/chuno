500a38c6eac3b92b5bc17ffd157a803ecf5ed2b8
Won-myoung <wonmyounglee@gmail.com>
2024-05-10 16:03:38 +0900

    메인페이지 태그 슬라이드 변경

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -6,7 +6,24 @@ import Header from '@components/layout/Header';
 import styles from './mainpage.module.css';
 import PlayListItem from '@pages/playlist/PlayListItem';
 
-function Mainpage() {
+/* swiper  추가시작 */
+// import React, { useRef } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import './styles.css';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
+/* swiper  import end */
+
+export default function Mainpage() {
   const axios = useCustomAxios();
   const [data, setData] = useState(null);
 
@@ -35,15 +52,55 @@ function Mainpage() {
         <span className={styles.miniTitle}>주인장 PICK!</span>
         <h2 className={styles.bodyText}>추천 플레이리스트</h2>
         <div className={styles.recommendList}>
-          <Recommendlistitem theme={'리듬타용'} value={'rythmn'} />
-          <Recommendlistitem theme={'놀러가용'} value={'play'} />
-          <Recommendlistitem theme={'행복해용'} value={'happy'} />
-          <Recommendlistitem theme={'우울해용'} value={'sad'} />
-          <Recommendlistitem theme={'집중해용'} value={'conp'} />
-          <Recommendlistitem theme={'멍때려용'} value={'mung'} />
-          <Recommendlistitem theme={'화가나용'} value={'anger'} />
-          <Recommendlistitem theme={'내꿈꿔용'} value={'dream'} />
-          <Recommendlistitem theme={'움직여용'} value={'act'} />
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={10}
+            loop={true}
+            autoplay={{ delay: 1000 }} // 3초마다 슬라이드가 자동으로 전환됨
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className={styles.mySwiper}
+          >
+            <SwiperSlide>
+              {' '}
+              <Recommendlistitem theme={'리듬타용'} value={'rythmn'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <Recommendlistitem theme={'놀러가용'} value={'play'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <Recommendlistitem theme={'행복해용'} value={'happy'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <Recommendlistitem theme={'우울해용'} value={'sad'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <Recommendlistitem theme={'집중해용'} value={'conp'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <Recommendlistitem theme={'멍때려용'} value={'mung'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <Recommendlistitem theme={'화가나용'} value={'anger'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <Recommendlistitem theme={'내꿈꿔용'} value={'dream'} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <Recommendlistitem theme={'움직여용'} value={'act'} />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
       <div className={styles.contents}>
@@ -62,4 +119,4 @@ function Mainpage() {
   );
 }
 
-export default Mainpage;
+
